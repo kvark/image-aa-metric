41768166654efdc75acad94438c69715732ec64a
Dzmitry Malyshau <kvarkus@gmail.com>
2015-06-13 12:47:38 -0400

    Redesigned the algorithm using the stars detection idea

--- src/main.rs
+++ src/main.rs
@@ -17,56 +17,39 @@ fn main() {
 
     let (width, height) = im.dimensions();
     println!("Dimensions are {}x{}", width, height);
-    
-    let mut sum1_hor = [0 as Count; 3];
-    let mut sum2_hor = [0 as Count; 3];
-    let mut sum1_ver = [0 as Count; 3];
-    let mut sum2_ver = [0 as Count; 3];
 
-    for y in 0..height {
-        for x in 0..width {
-            let pixel = im.get_pixel(x, y);
-            if x > 0 {
-                let prev = im.get_pixel(x-1, y);
-                for k in 0..3 {
-                    let diff = ((pixel.data[k] as i32) - (prev.data[k] as i32)).abs() as Count;
-                    sum1_hor[k] += diff;
-                    sum2_hor[k] += diff*diff;
-                }
-            }
-            if y > 0 {
-                let prev = im.get_pixel(x, y-1);
-                for k in 0..3 {
-                    let diff = ((pixel.data[k] as i32) - (prev.data[k] as i32)).abs() as Count;
-                    sum1_ver[k] += diff;
-                    sum2_ver[k] += diff*diff;
-                }
+    if width < 2 || height < 2 {
+        return;
+    }
+
+    fn get_score(a: u8, b: u8, x: u8) -> f32 {
+        let sigma = ((a as i32) - (b as i32)).abs();
+        let avg = ((a as i32) + (b as i32)) >> 1;
+        let smoother = 5f32;
+        let divisor = smoother / (sigma as f32 + smoother);
+        let relative = (x as i32 - avg).abs() as f32 * divisor;
+        let base = 2f32;
+        (relative - base).max(0.0)
+    }
+
+    let mut score = [0f32; 3];
+
+    for y in 1..height {
+        for x in 1..width {
+            let a = im.get_pixel(x-1, y-1);
+            let b = im.get_pixel(x, y-1);
+            let c = im.get_pixel(x-1, y);
+            let d = im.get_pixel(x, y);
+            for k in 0..3 {
+                score[k] += get_score(b.data[k], c.data[k], a.data[k]);
+                score[k] += get_score(b.data[k], c.data[k], d.data[k]);
+                score[k] += get_score(a.data[k], d.data[k], b.data[k]);
+                score[k] += get_score(a.data[k], d.data[k], c.data[k]);
             }
         }
     }
 
-    fn print_metric(data: [Count; 3], total: u32, power: f32, name: &str) {
-        let mut rel = [0f32; 3];
-        for (r, d) in rel.iter_mut().zip(data.iter()) {
-            *r =  ((*d as f32) / (total as f32)).powf(power);
-        }
-        println!("{}: {:2}, {:2}, {:2}", name, rel[0], rel[1], rel[2]);
-    }
-
-    let total_hor = (width - 1) * height;
-    let total_ver = width * (height - 1);
-	let sum1 = sum1_hor.iter().chain(sum1_ver.iter()).fold(0, |u, &f| u + f);
-	let sum2 = sum2_hor.iter().chain(sum2_ver.iter()).fold(0, |u, &f| u + f);
-	let total = ((total_hor + total_ver) * 3) as f32;
-
-	if false {
-		println!("Detail AA metrics:");
-		print_metric(sum1_hor, total_hor, 1.0, "\tHorisontal pow1");
-		print_metric(sum2_hor, total_hor, 0.5, "\tHorisontal pow2");
-		print_metric(sum1_ver, total_ver, 1.0, "\tVertical pow1");
-		print_metric(sum2_ver, total_ver, 0.5, "\tVertical pow2");
-	}
-
-	println!("Avg pow1: {:2}, pow2: {:2}", (sum1 as f32) / total,
-		((sum2 as f32) / total).sqrt());
+    let total = (width - 1) * (height - 1);
+    let sum = score.iter().fold(0f32, |u, &s| u + s) / (total as f32);
+    println!("AA metric score: {:2}", sum);
 }
